a86d07e57fe947dc75ef927461ae7be93c3b1e99
Yukihide Takahashi <yukihide99@gmail.com>
2022-12-21 13:51:30 +0800

    styling for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import { header } from "./Header.module.css";
+import { header, title } from "./Header.module.css";
 function Header() {
   return (
-    <>
-      <h1 className={header}>株式会社ゆめみ入社試験</h1>
+    <div className={header}>
+      <h1 className={title}>株式会社ゆめみ入社試験</h1>
       <h2>都道府県別人口推移</h2>
-    </>
+    </div>
   );
 }
 
