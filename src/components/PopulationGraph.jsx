f4117343762ed3e8feee5c0205f884e7474750f0
--global <--global>
2022-12-25 09:15:58 +0800

    add testing for population graph

--- src/components/PopulationGraph.jsx
+++ src/components/PopulationGraph.jsx
@@ -41,45 +41,47 @@ function PopulationGraph({ result, message }) {
         最終更新日:
         {new Date().toLocaleString('en-US', { timeZone: 'Asia/Tokyo' }).slice(0, 10)}
       </h3>
-      <LineChart
-        margin={{
-          top: 50,
-          right: 10,
-          left: 10,
-          bottom: 5,
-        }}
-        data={result}
-        width={800}
-        height={600}
-      >
-        <YAxis
-          tickFormatter={(value) => {
-            return new Intl.NumberFormat('en-US', {
-              notation: 'compact',
-              compactDisplay: 'short',
-            }).format(value);
+      <div data-testid='line-chart'>
+        <LineChart
+          margin={{
+            top: 50,
+            right: 10,
+            left: 10,
+            bottom: 5,
           }}
-          type='number'
-          width={100}
-          stroke='black'
+          data={result}
+          width={800}
+          height={600}
         >
-          <Label value='人口' position='insideLeft' angle={270} fill='black' />
-        </YAxis>
-        <XAxis
-          padding={{ left: 5, right: 5 }}
-          tickCount={10}
-          angle={-60}
-          height={90}
-          dataKey='year'
-          stroke='black'
-        >
-          <Label value='年度' position='insideBottom' fill='black' />
-        </XAxis>
-        <CartesianGrid stroke='#ffffff' />
-        <Tooltip content={<CustomTooltip />} />
-        <Legend />
-        {lines(result)}
-      </LineChart>
+          <YAxis
+            tickFormatter={(value) => {
+              return new Intl.NumberFormat('en-US', {
+                notation: 'compact',
+                compactDisplay: 'short',
+              }).format(value);
+            }}
+            type='number'
+            width={100}
+            stroke='black'
+          >
+            <Label value='人口' position='insideLeft' angle={270} fill='black' />
+          </YAxis>
+          <XAxis
+            padding={{ left: 5, right: 5 }}
+            tickCount={10}
+            angle={-60}
+            height={90}
+            dataKey='year'
+            stroke='black'
+          >
+            <Label value='年度' position='insideBottom' fill='black' />
+          </XAxis>
+          <CartesianGrid stroke='#ffffff' />
+          <Tooltip content={<CustomTooltip />} />
+          <Legend />
+          {lines(result)}
+        </LineChart>
+      </div>
     </div>
   );
 }
---
+++ src/components/PopulationGraph.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import PopulationGraph from './PopulationGraph';
+
+describe('PopulationGraph', () => {
+  it('checks the value of last updated date', () => {
+    const today = new Date().toLocaleString('en-US', { timeZone: 'Asia/Tokyo' }).slice(0, 10);
+    const { getByText } = render(<PopulationGraph />);
+    const h3value = getByText(`最終更新日:${today}`);
+    expect(h3value).toBeInTheDocument();
+  });
+
+  it('checks existance of line-chart', () => {
+    const { queryByTestId } = render(<PopulationGraph />);
+    const lineChart = queryByTestId('line-chart');
+    expect(lineChart).toBeInTheDocument();
+  });
+});
