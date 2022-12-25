ff1ea1ae51f87a66234a11636873919dbe98633b
--global <--global>
2022-12-25 10:31:52 +0800

    added waitfor for app test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import { waitFor, render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('renders App component', () => {
-    render(<App />);
+  it('renders App component', async () => {
+    await waitFor(() => render(<App />));
   });
 });
