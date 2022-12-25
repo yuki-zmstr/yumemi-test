90d2c35c3264ea697c4771602c1eab3ae687e5b6
--global <--global>
2022-12-25 09:47:20 +0800

    add choose prefectures test

---
+++ src/components/ChoosePrefectures.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import ChoosePrefectures from './ChoosePrefectures';
+
+describe('ChoosePrefectures', () => {
+  it('renders ChoosePrefectures component', () => {
+    render(<ChoosePrefectures />);
+  });
+});
