164cd5fd5ef5158ba51c4cbeaac88953d00b19d9
--global <--global>
2022-12-25 18:46:44 +0800

    added responsive container

--- src/components/PopulationGraph.jsx
+++ src/components/PopulationGraph.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { LineChart, XAxis, CartesianGrid, Line, Tooltip, YAxis, Label, Legend } from 'recharts';
+import {
+  LineChart,
+  XAxis,
+  CartesianGrid,
+  Line,
+  Tooltip,
+  YAxis,
+  Label,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
 
 import styles from '../stylesheets/PopulationGraph.module.css';
 
@@ -41,46 +51,49 @@ function PopulationGraph({ result, message }) {
         最終更新日:
         {new Date().toLocaleString('en-US', { timeZone: 'Asia/Tokyo' }).slice(0, 10)}
       </h3>
-      <div data-testid='line-chart'>
-        <LineChart
-          margin={{
-            top: 50,
-            right: 10,
-            left: 10,
-            bottom: 5,
-          }}
-          data={result}
-          width={800}
-          height={600}
-        >
-          <YAxis
-            tickFormatter={(value) => {
-              return new Intl.NumberFormat('en-US', {
-                notation: 'compact',
-                compactDisplay: 'short',
-              }).format(value);
+
+      <div data-testid='line-chart' className={styles.lineChart}>
+        <ResponsiveContainer>
+          <LineChart
+            margin={{
+              top: 50,
+              right: 10,
+              left: 10,
+              bottom: 5,
             }}
-            type='number'
-            width={100}
-            stroke='black'
+            data={result}
+            width={800}
+            height={600}
           >
-            <Label value='人口' position='insideLeft' angle={270} fill='black' />
-          </YAxis>
-          <XAxis
-            padding={{ left: 5, right: 5 }}
-            tickCount={10}
-            angle={-60}
-            height={90}
-            dataKey='year'
-            stroke='black'
-          >
-            <Label value='年度' position='insideBottom' fill='black' />
-          </XAxis>
-          <CartesianGrid stroke='#ffffff' />
-          <Tooltip content={<CustomTooltip />} />
-          <Legend />
-          {lines(result)}
-        </LineChart>
+            <YAxis
+              tickFormatter={(value) => {
+                return new Intl.NumberFormat('en-US', {
+                  notation: 'compact',
+                  compactDisplay: 'short',
+                }).format(value);
+              }}
+              type='number'
+              width={100}
+              stroke='black'
+            >
+              <Label value='人口' position='insideLeft' angle={270} fill='black' />
+            </YAxis>
+            <XAxis
+              padding={{ left: 5, right: 5 }}
+              tickCount={10}
+              angle={-60}
+              height={90}
+              dataKey='year'
+              stroke='black'
+            >
+              <Label value='年度' position='insideBottom' fill='black' />
+            </XAxis>
+            <CartesianGrid stroke='#ffffff' />
+            <Tooltip content={<CustomTooltip />} />
+            <Legend />
+            {lines(result)}
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
