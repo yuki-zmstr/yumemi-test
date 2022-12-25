9d36d5cf3f89b6283bcce8a82de2a6c6b02624e5
--global <--global>
2022-12-25 18:46:31 +0800

    removed unnecessary tests

--- src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { waitFor, render } from '@testing-library/react';
-import App from './App';
-
-describe('App', () => {
-  it('renders App component', async () => {
-    await waitFor(() => render(<App />));
-  });
-});
--- src/components/Body.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { waitFor, render } from '@testing-library/react';
-import Body from './Body';
-
-describe('Body', () => {
-  it('renders Body component', async () => {
-    await waitFor(() => render(<Body />));
-  });
-});
