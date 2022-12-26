842a39e55efb4aabb26917d6638de3632b817f0f
yuki-zmstr <yukihide99@gmail.com>
2022-12-26 16:13:23 +0800

    added comments for chooseprefectures.test.js

--- src/components/ChoosePrefectures.test.jsx
+++ src/components/ChoosePrefectures.test.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 
 import ChoosePrefectures from './ChoosePrefectures';
 
 describe('ChoosePrefectures', () => {
-  it('renders ChoosePrefectures component', () => {
-    render(<ChoosePrefectures />);
+  it('renders ChoosePrefectures component', async () => {
+    await waitFor(() => render(<ChoosePrefectures />));
   });
 });
+
+// api の呼び出しのテストコードを書くところで詰まっています。
+// https://vhudyma-blog.eu/3-ways-to-mock-axios-in-jest/#Way-1-jest.mock()
+// これに従ってテストコードを書くと、instance is not a function エラーが出ます ><
+// api.js 内の instance 関数を export すればいいかなと思ったのですが、import しても結果変わらず。。。
+// ぜひ相談させてください！かなり重要なテストだと思うので。僕自身勉強したいです。
+
+// これができれば、getPrefectures と getPopulationData の呼び出しのテストができます。
